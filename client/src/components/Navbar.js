6efb874070d08c1b66c370d9cab85071b4679e08
prince475 <danielmwai475@gmail.com>
2023-05-28 00:29:45 +0300

    Creating a Navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import logo from '../images/airbnb-logo-0.png'
+
+function Navbar() {
+    return (
+
+            <nav className='navbar'>
+                {/* <img className='nav---image' src={logo} alt="airbnb logo"/> */}
+                <h1 className='nav---item'>Smart Cart</h1>
+            </nav>
+
+    )
+
+
+}
+export default Navbar;
